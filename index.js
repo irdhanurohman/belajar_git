0b2dbcb8ed297eecf8c0a166f97163d5884db7b9
azmanabdullah <azmanabdullahmances@gmail.com>
2019-11-30 23:08:34 +0900

    add jobs for everyone

--- index.js
+++ index.js
@@ -32,15 +32,58 @@ const mataKuliah = [
    },
 ]
 
+
+const jobs = [
+   {
+      _id: 1,
+      job:"programmer",
+      mahasiswa_id : [1,2],
+   },
+   {
+      _id: 2,
+      job:"Kontrusi bangunan",
+      mahasiswa_id : 3,
+   }
+];
+
 function relationMatkul(id)
 {
    return mataKuliah.filter(val => val._id == id)[0].title;
 }
 
 
+function relationJobs(id)
+{
+   return jobs.filter(val => {
+      if(checkArray(val.mahasiswa_id))
+      {
+         return val.mahasiswa_id.includes(id) && val.mahasiswa_id;
+      }else{
+         return  val.mahasiswa_id;
+      }
+
+
+   })[0].job;
+}
+
+
+function checkArray(array)
+{
+   if(array.length > 0)
+   {
+      return true;
+   }else{
+      return false;
+   }
+}
+
+
+
+
+
 // console.log(relationMatkul(1));
 mahasiswa.map(val => {
-   
-      console.log(`mahasiswa ${val.nama} punya matkul -> ${relationMatkul(val._id)}`);
+
+      console.log(`mahasiswa ${val.nama} punya job -> ${relationJobs(val._id)}`);
       console.log(`------------`);
 });
