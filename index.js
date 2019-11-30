1a3d5b04a8e35672d84fc79ff24eb0d5c1c4d63f
azmanabdullah <azmanabdullahmances@gmail.com>
2019-11-30 22:10:48 +0900

    add users _id for everyone

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const mahasiswa = [
   {
+    _id: 1,
     nama:"irdha",
     nim:89
   },
     {
+      _id:2,
     nama:"azman",
     nim:12
   },
   {
+     _id:3,
      nama:"sandhika",
      nim : 99
  }
 
 ];
+
+const mataKuliah = [
+   {
+      title:"Algoritma dan pemograman",
+      _id:1
+   },
+   {
+      title:"jaringan komputer",
+      _id:3
+   },
+   {
+      title:"Basis Data",
+      _id:2
+   },
+]
 
 console.log(mahasiswa[2].nama);
